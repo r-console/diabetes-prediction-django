499e9504e010bb30d28fafb1aa5d337fc9adc6a5
r-console <rameshsolaimalai878@gmail.com>
2023-06-03 13:14:21 +0530

    vercel host setup

--- Diabetesapp/settings.py
+++ Diabetesapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^n5msy5$p+mn%+m&nq9o_887&4w15jg+usikq=u^87&xxl6v%s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 # Application definition
 
